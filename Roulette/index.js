4e8e182f9638bc567d6f306798024c0e2d569a9c
K <furan0004@gmail.com>
2022-08-15 03:54:57 +0900

    fix test 4

--- Roulette/index.js
+++ Roulette/index.js
@@ -33,7 +33,8 @@ var time;
                 console.log(deg, time, deg / 180);
                 for(let i = 0; i < list.children.length; i++){
                     if(pos > deg / 180){
-                        console.log(list.children[i - 1].children[0].innerText, field.children[i - 1].style.borderTop.match("(rgb)\(.*\)")[2]);
+                        let rgb = field.children[i - 1].style.borderTop.match("(rgb)\(.*\)")[2].replace(".*[\(|\)].*", "").split(",");
+                        console.log(list.children[i - 1].children[0].innerText, rgb);
                         break;
                     }
 
