d53aa8e4148c980f7cc8c85046e20564d5575e91
K <furan0004@gmail.com>
2022-08-14 23:11:56 +0900

    fix test 2

--- Roulette/index.js
+++ Roulette/index.js
@@ -21,7 +21,7 @@ var field = document.getElementsByClassName("roulette-field")[0];
         let vertical = radius;
         let horizontal = vertical * Math.tan(angle / 2);
         let rgb = colourConverter.HSVtoRGB(360 * Math.random(), 1, 1);
-        let colour = colourConverter.RGBtoColourCode(rgb.r, rgb.g, rgb.b);
+        let colour = `rgb(${256 * rgb.r}, ${256 * rgb.g}, ${256 * rgb.b})`;
 
         console.log(rgb);
 
