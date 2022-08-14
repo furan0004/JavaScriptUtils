2d34493a766788e2eec173152596195c2ec43456
K <furan0004@gmail.com>
2022-08-15 04:21:10 +0900

    fix test 4

--- Roulette/index.js
+++ Roulette/index.js
@@ -29,7 +29,7 @@ var time;
 
             case 1:
                 let a = 0;
-                for(let i = 0; i < list.children.length; i++) a += list.children[i].children[1].value;
+                for(let i = 0; i < list.children.length - 1; i++) a += list.children[i].children[1].value;
                 let deg = (time / 25 * 36 - 180 * list.children[0].children[1].value / a) % 360;
                 let pos = 0;
                 console.log(deg, time, deg / 180);
