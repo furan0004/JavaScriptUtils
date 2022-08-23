4819478ac12c98a77dd85b228a3f6b22adce61dc
K <furan0004@gmail.com>
2022-08-24 06:33:47 +0900

    some fix and add data

--- scripts/univ.js
+++ scripts/univ.js
@@ -3,7 +3,6 @@ import topbar from "https://pages.kurosaki.love/scripts/index.js";
 function fixTopBar(title){
     topbar.setTitle(title || document.title || "黒咲くろんの手作り");
     topbar.setHome("https://utils.kurosaki.love");
-    topbar.setMenu([]);
     topbar.load();
 }
 
