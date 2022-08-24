27aca09f67fc8437a1434adf2f64e08366801108
K <furan0004@gmail.com>
2022-08-24 17:01:45 +0900

    continued test

--- s/link/hitobito/index.js
+++ s/link/hitobito/index.js
@@ -1,4 +1,4 @@
-import { buildPage } from "/litlinkcopy/pagebuild";
+import { buildPage } from "/litlinkcopy/pagebuild.js";
 import hitobitoData from "./hitobito.json" assert {type: "json"};
 
 buildPage(hitobitoData);
