df8123f1c6cc88debe13bdbafa75d1c21e5b5f6c
K <furan0004@gmail.com>
2022-08-24 11:14:06 +0900

    continued test

--- litlinkcopy/pagebuild.js
+++ litlinkcopy/pagebuild.js
@@ -5,17 +5,21 @@ import itemList from "./styles/master.json" assert {type: "json"};
 (function(){
     importItemmStyles();
 
-
 })();
 
 
 async function importItemmStyles(){
     for(let i = 0; i < itemList.length; i++){
-        let style = await import(itemList[i].path, {assert: {type: "css"}}).then(module => {
-            console.log(module);
-            document.adoptedStyleSheets.push(module);
-        }).catch(err => {
-            console.log(err.message);
-        });
+        let style = getStyle(itemList[i].path);
+        console.log(style);
+        document.adoptedStyleSheets.push(style);
     }
+}
+
+async function getStyle(path){
+    const {default: aaa} = await import(path, {assert: {type: "css"}}).then(module => {
+        console.log(module);
+    }).catch(err => {
+        console.log(err.message);
+    });
 }
