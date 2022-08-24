8b521bff0db5ffc2523e6632f3010265cc4d5a35
K <furan0004@gmail.com>
2022-08-24 18:18:25 +0900

    fix

--- litlinkcopy/pagebuild.js
+++ litlinkcopy/pagebuild.js
@@ -155,6 +155,7 @@ function createItem(info){
                 item.classList.add("dynamic-pane");
                 item.style.background = (info.background.substring(0, 1) == "#") ? info.background : `url(${info.background})`;
                 item.style.backgroundSize = "cover";
+                item.style.backgroundPosition = "center";
                 item.style.width = "100%";
                 item.style.padding = "0";
 
--- s/link/hitobito/index.js
+++
@@ -1,4 +0,0 @@
-import { buildPage } from "/litlinkcopy/pagebuild.js";
-import hitobitoData from "./hitobito.json" assert {type: "json"};
-
-buildPage(hitobitoData);
