1a981ced2064f941a0d8be810c2cb469f7b10cd8
K <furan0004@gmail.com>
2022-08-24 11:30:21 +0900

    continued test

--- litlinkcopy/pagebuild.js
+++ litlinkcopy/pagebuild.js
@@ -6,12 +6,17 @@ import itemStyle_0 from "/litlinkcopy/styles/simple_row.css" assert {type: "css"
 import itemStyle_1 from "/litlinkcopy/styles/dynamic_pane.css" assert {type: "css"};
 import itemStyle_2 from "/litlinkcopy/styles/string_pane.css" assert {type: "css"};
 
-document.adoptedStyleSheets(itemStyle_0);
-document.adoptedStyleSheets(itemStyle_1);
-document.adoptedStyleSheets(itemStyle_2);
+document.adoptedStyleSheets.push(itemStyle_0);
+document.adoptedStyleSheets.push(itemStyle_1);
+document.adoptedStyleSheets.push(itemStyle_2);
+
+//import main css
+import indexStyle from "./index.css" assert {type: "css"};
+document.adoptedStyleSheets.push(indexStyle);
 
 
 (function(){
+    document.title = pageData.title || `${pageData.name}のプロフィール`;
 
 })();
 
