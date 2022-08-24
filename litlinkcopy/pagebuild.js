a5e647ffa4c4001d8989268e85f5d21ab97aaa34
K <furan0004@gmail.com>
2022-08-24 10:54:36 +0900

    test
    lit link copy

---
+++ litlinkcopy/pagebuild.js
@@ -0,0 +1,20 @@
+import pageData from "pagedata.json" assert {type: "json"};
+import itemList from "./styles/master.json" assert {type: "json"};
+
+
+(function(){
+    importItemmStyles();
+
+    
+})();
+
+
+async function importItemmStyles(){
+    for(let i = 0; i < itemList.length; i++){
+        let style = await import(itemList[i].path).then(module => {
+            document.adoptedStyleSheets.push(style);
+        }).catch(err => {
+            console.log(err.message);
+        });
+    }
+}
