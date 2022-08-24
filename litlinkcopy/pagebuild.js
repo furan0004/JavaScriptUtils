6c8dae2221ba4e4b02ec175d1c746599165f787b
K <furan0004@gmail.com>
2022-08-24 11:27:09 +0900

    continued test

--- litlinkcopy/pagebuild.js
+++ litlinkcopy/pagebuild.js
@@ -1,25 +1,23 @@
 import pageData from "./pagedata.json" assert {type: "json"};
 import itemList from "./styles/master.json" assert {type: "json"};
 
+//import css
+import itemStyle_0 from "/litlinkcopy/styles/simple_row.css" assert {type: "css"};
+import itemStyle_1 from "/litlinkcopy/styles/dynamic_pane.css" assert {type: "css"};
+import itemStyle_2 from "/litlinkcopy/styles/string_pane.css" assert {type: "css"};
+
+document.adoptedStyleSheets(itemStyle_0);
+document.adoptedStyleSheets(itemStyle_1);
+document.adoptedStyleSheets(itemStyle_2);
+
 
 (function(){
-    importItemmStyles();
 
 })();
 
 
 async function importItemmStyles(){
     for(let i = 0; i < itemList.length; i++){
-        let style = getStyle(itemList[i].path);
-        console.log(style);
-        document.adoptedStyleSheets.push(style.default.default);
+        
     }
-}
-
-async function getStyle(path){
-    const {default: aaa} = await import(path, {assert: {type: "css"}}).then(module => {
-        console.log(module);
-    }).catch(err => {
-        console.log(err.message);
-    });
 }
