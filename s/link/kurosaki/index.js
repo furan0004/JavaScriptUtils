b2bd3bfc34218981cedd41e5c63d303e0fd90ef5
K <furan0004@gmail.com>
2022-08-28 17:04:20 +0900

    fix

--- s/link/kurosaki/index.js
+++ s/link/kurosaki/index.js
@@ -1,11 +1,3 @@
-import kurosakiData from  "./kurosaki.json" assert {type: "json"};
-/*
-import { buildPage } from "/litlinkcopy/pagebuild.js";
-
-buildPage(kurosakiData);
-
-*/
-
 import { ProfilePage } from "/lib/ProfilePageBuilder/builder.js";
 var profile = new ProfilePage("https://utils.kurosaki.love/s/link/kurosaki/kurosaki.json");
 profile.complete();
