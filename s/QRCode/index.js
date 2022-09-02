88878d07c83bbdd26458e1a10f6037e7def8eb66
K <furan0004@gmail.com>
2022-09-02 09:17:02 +0000

    added source code

--- s/QRCode/index.js
+++ s/QRCode/index.js
@@ -1,2 +1,17 @@
 import indexStyle from "./index.css" assert {type: "css"};
 document.adoptedStyleSheets.push(indexStyle);
+
+var qrcodeDisplay = document.getElementById("qrcodeDisplay");
+
+var textarea = document.getElementById("subject");
+var qrSize = document.getElementById("qrSize");
+
+(function(){
+    textarea.addEventListener("change", function(){
+        let text = textarea.value;
+
+        qrcodeDisplay.src = `https://chart.googleapis.com/chart?cht=qr&chs=${qrSize.value}&chl=${encodeURI(text)}`;
+    });
+
+    textarea.value = "";
+})();
